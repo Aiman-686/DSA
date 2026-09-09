395b28fe7b1fc43b162d406640511527c01252a3
Aiman-686 <119659489+Aiman-686@users.noreply.github.com>
2026-09-09 13:27:44 +0530

    Time: 3 ms (24.50%) | Memory: 7.9 MB (39.70%) - LeetSync

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int flips = 0;
+        while(a>0 || b > 0 || c > 0){
+            int bitA = a & 1;
+            int bitB = b & 1;
+            int bitC = c & 1;
+            // we want OR = 1
+            if(bitC == 1){
+                //Both are 0, so flip one
+                if(bitA == 0 && bitB == 0){
+                    flips++;
+                }
+            }
+            //we want OR = 0 
+            else{
+                //Both must become 0
+                if(bitA == 1){
+                    flips++;
+                }
+                if(bitB == 1){
+                    flips++;
+                }
+        
+            }//move to next bit
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+
+        }return flips++;
+    }
+};
